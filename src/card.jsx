e952e44098c06d2e0ff70ba8e542c35c66863cdb
Oleh Rytov <oleja.229@gmail.com>
2024-04-03 23:50:55 +0300

    created a movie website, which gives you a short info about the film get recommended/are looking for

--- src/card.jsx
+++ src/card.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 
-const Card = () =>{
+const Card = (movie) =>{
+    console.log(movie.info)
+    let img_path = "https://image.tmdb.org/t/p/w500"
     return (
         <>
         <div className="movie">
-            <img src="./public/psycho.jpg" className="poster"></img>
+            <img src={img_path+movie.info.poster_path} className="poster"></img>
             <div className="movie-details">
                 <div className="box">
-                    <h4 className="title">Title</h4>
-                    <p className="rating">9.0</p>
+                    <h4 className="title">{movie.info.title}</h4>
+                    <p className="rating">{movie.info.vote_average}</p>
                 </div>
                 <div className="overview">
                     <h1>Overview</h1>
-                    <p>"Psycho" (1960), directed by Alfred Hitchcock, is a groundbreaking thriller about a secretary, a secluded motel, and its mysterious owner, Norman Bates.</p>
+                    <p>{movie.info.overview}</p>
                 </div>
             </div>
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,24 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Card from "./card";
 
+let API_key = "&api_key=110e459ff6ce307eee36bf34d1351ce3";
+let base_url = "https://api.themoviedb.org/3";
+let url = base_url + "/discover/movie?sort_by=popularity.desc" + API_key;
+let arr = ["Popular", "Theatre", "Kids", "Drama", "Comedy"];
+
 const Main = () => {
+  const [movieData, setMovieData] = useState([]);
+  const [url_set, setUrl] = useState(url);
+  const [search, setSearch] = useState();
+
+  useEffect(() => {
+    fetch(url_set)
+      .then((res) => res.json())
+      .then((data) => {
+        setMovieData(data.results);
+      });
+  }, [url_set]);
+
   useEffect(() => {
     const searchInput = document.getElementById("searchInput");
 
@@ -12,34 +29,69 @@ const Main = () => {
         searchInput.classList.remove("notEmpty");
       }
     };
-      searchInput.addEventListener("input", handleInput);
+    searchInput.addEventListener("input", handleInput);
 
-      return () => {
-        searchInput.removeEventListener("input", handleInput)
-      
-
+    return () => {
+      searchInput.removeEventListener("input", handleInput);
     };
   }, []);
+  const getData = (movieType) => {
+    if (movieType == "Popular") {
+      url = base_url + "/discover/movie?sort_by=popularity.desc" + API_key;
+    }
+    if (movieType == "Theatre") {
+      url =
+        base_url +
+        "/discover/movie?primary_release_date.gte=2014-09-15&primary_release_date.lte=2014-10-22" +
+        API_key;
+    }
+    if (movieType == "Kids") {
+      url =
+        base_url +
+        "/discover/movie?certification_country=US&certification.lte=G&sort_by=popularity.desc" +
+        API_key;
+    }
+    if (movieType == "Drama") {
+      url =
+        base_url +
+        "/discover/movie?with_genres=18&primary_release_year=2014" +
+        API_key;
+    }
+    if (movieType == "Comedy") {
+      url =
+        base_url +
+        "/discover/movie?with_genres=35&with_cast=23659&sort_by=revenue.desc" +
+        API_key;
+    }
+    setUrl(url);
+  };
+  const searchMovie = (evt) =>{
+    if(evt.key == "Enter"){
+        url=base_url+"/search/movie?api_key=110e459ff6ce307eee36bf34d1351ce3&query="+search;
+        setUrl(url);
+        setSearch(" ");
+    }
+  }
   return (
     <>
       <div className="header">
         <nav>
           <ul>
-            <li>
-              <a href="#">Popular</a>
-            </li>
-            <li>
-              <a href="#">Theatre</a>
-            </li>
-            <li>
-              <a href="#">Kids</a>
-            </li>
-            <li>
-              <a href="#">Drama</a>
-            </li>
-            <li>
-              <a href="#">Comedy</a>
-            </li>
+            {arr.map((value) => {
+              return (
+                <li>
+                  <a
+                    href="#"
+                    name={value}
+                    onClick={(e) => {
+                      getData(e.target.name);
+                    }}
+                  >
+                    {value}
+                  </a>
+                </li>
+              );
+            })}
           </ul>
         </nav>
         <form>
@@ -49,6 +101,11 @@ const Main = () => {
               placeholder="Movie name"
               className="inputText"
               id="searchInput"
+              onChange={(e) => {
+                setSearch(e.target.value);
+              }}
+              value={search}
+              onKeyUp={searchMovie}
             ></input>
             <button type="search" className="search-btn">
               <i className="fa-solid fa-magnifying-glass"></i>
@@ -57,11 +114,17 @@ const Main = () => {
         </form>
       </div>
       <div className="container">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {movieData.length == 0 ? (
+          <p className="notfound">Not Found</p>
+        ) : (
+          movieData.map((res, pos) => {
+            return (
+              <>
+                <Card info={res} key={pos} />
+              </>
+            );
+          })
+        )}
       </div>
     </>
   );
